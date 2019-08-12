58838193796f3a31d192c114d715b79f27b05622
Edward Garmon <garmonedward@gmail.com>
2019-08-12 16:22:48 -0500

    initial commit

---
+++ .next/static/webpack/static/development/pages/index.js.c3307ff31d596a099561.hot-update.js
@@ -0,0 +1,53 @@
+webpackHotUpdate("static\\development\\pages\\index.js",{
+
+/***/ "./node_modules/process/browser.js":
+false,
+
+/***/ "./node_modules/string-hash/index.js":
+false,
+
+/***/ "./node_modules/styled-jsx/dist/lib/stylesheet.js":
+false,
+
+/***/ "./node_modules/styled-jsx/dist/style.js":
+false,
+
+/***/ "./node_modules/styled-jsx/dist/stylesheet-registry.js":
+false,
+
+/***/ "./node_modules/styled-jsx/style.js":
+false,
+
+/***/ "./pages/index.js":
+/*!************************!*\
+  !*** ./pages/index.js ***!
+  \************************/
+/*! exports provided: default */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+/* harmony import */ var react__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! react */ "./node_modules/react/index.js");
+/* harmony import */ var react__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(react__WEBPACK_IMPORTED_MODULE_0__);
+var _jsxFileName = "C:\\Users\\garmo\\Documents\\Websites\\NextTut\\pages\\index.js";
+
+
+function HiThere() {
+  return react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("p", {
+    style: {
+      color: 'red'
+    },
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 2
+    },
+    __self: this
+  }, "hi there");
+}
+
+/* harmony default export */ __webpack_exports__["default"] = (HiThere);
+
+/***/ })
+
+})
+//# sourceMappingURL=index.js.c3307ff31d596a099561.hot-update.js.map
