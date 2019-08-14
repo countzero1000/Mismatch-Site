adb498969612bec86192f4ef93363fe0bf3e1fd0
Edward Garmon <garmonedward@gmail.com>
2019-08-14 15:17:08 -0500

    added prestart script for heroku

--- components/navbar.js
+++ components/navbar.js
@@ -3,7 +3,7 @@ import Link from 'next/link';
 const Navbar = () => (
   <nav className="navbar navbar-expand navbar-dark bg-dark mb-4">
     <div className="container">
-      <a className="navbar-brand" href="#">MismatchMedia</a>
+      <a className="navbar-brand" href="#">Mismatch Media</a>
       <div className="collapse navbar-collapse">
         <ul className="navbar-nav ml-auto">
           <li className="nav-item">
